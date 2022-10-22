50bf6da0b6bb390a43e121985d941c2eb111f202
Santiago Canovari <santiagocanovari@gmail.com>
2022-10-22 01:21:16 -0300

    Created db model

--- project/FinalProject/AppFinalProject/models.py
+++ project/FinalProject/AppFinalProject/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+class Groups(models.Model):
+    group = models.CharField(max_length=30)
+
+class User(models.Model):
+    username = models.CharField(max_length=18)
+    password = models.CharField(max_length=22)
+    email = models.CharField(max_length=300, default="placeholder.jpg")
+    icon = models.CharField(max_length=300)
+    group = models.ForeignKey(Groups, on_delete=models.CASCADE)
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.CharField(max_length=300)
+
+class Post(models.Model):
+
+    writter = models.IntegerField()
+    votes=models.IntegerField()
+    text=models.CharField(max_length=300)
+    image=models.CharField(max_length=300, null=True)
+    comments = models.ManyToManyField(Comment)
+
+
--- project/FinalProject/FinalProject/settings.py
+++ project/FinalProject/FinalProject/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'AppFinalProject.apps.AppfinalprojectConfig'
+    'AppFinalProject'
 ]
 
 MIDDLEWARE = [
