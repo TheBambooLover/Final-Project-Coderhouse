bf00b966711e0808ed167ed2c99fd8f022eb57b0
Tomas Oldenburg <oldenburgtomas@hotmail.com>
2022-10-24 16:52:16 -0300

    add bootstrap and navbar

--- project/FinalProject/AppFinalProject/urls.py
+++ project/FinalProject/AppFinalProject/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from AppFinalProject import views
 
 urlpatterns = [
-    path('home/',views.home),
+    path('index/',views.home),
     path('posts/',views.posts),
-    path('writters/',views.writters),
+    path('contact/',views.conctact),
     path('about/',views.about),
 
 ]
--- project/FinalProject/AppFinalProject/views.py
+++ project/FinalProject/AppFinalProject/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, "AppFinalProject/home.html")
+    return render(request, "AppFinalProject/index.html")
 
 def posts(request):
         return render(request, "AppFinalProject/posts.html")
 
-def writters(request):
-        return render(request, "AppFinalProject/writters.html")
+def conctact(request):
+        return render(request, "AppFinalProject/contact.html")
 
 def about(request):
         return render(request, "AppFinalProject/about.html")
