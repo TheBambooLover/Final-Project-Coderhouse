9f459c41892a5412bf52bb151d995cd0e9046a71
Santiago Canovari <santiagocanovari@gmail.com>
2022-10-25 21:31:43 -0300

    (feat) Added admin panel and reformated DB

--- project/FinalProject/AppFinalProject/admin.py
+++ project/FinalProject/AppFinalProject/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import *
+
+admin.site.register(Groups)
+
+admin.site.register(User)
+
+admin.site.register(Comment)
+
+admin.site.register(Post)
 
 # Register your models here.
--- project/FinalProject/AppFinalProject/models.py
+++ project/FinalProject/AppFinalProject/models.py
@@ -1,3 +1,6 @@
+from tkinter import CASCADE
+from unittest.mock import DEFAULT
+from certifi import where
 from django.db import models
 
 class Groups(models.Model):
@@ -6,20 +9,17 @@ class Groups(models.Model):
 class User(models.Model):
     username = models.CharField(max_length=18)
     password = models.CharField(max_length=22)
-    email = models.CharField(max_length=300, default="placeholder.jpg")
-    icon = models.CharField(max_length=300)
+    email = models.CharField(max_length=300)
+    icon = models.CharField(max_length=300, default="placeholder.jpg")
     group = models.ForeignKey(Groups, on_delete=models.CASCADE)
+
+class Post(models.Model):
+    writter = models.ForeignKey(User, on_delete=models.CASCADE)
+    votes=models.IntegerField()
+    text=models.CharField(max_length=300)
+    image=models.CharField(max_length=300, null=True)
 
 class Comment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     text = models.CharField(max_length=300)
-
-class Post(models.Model):
-
-    writter = models.IntegerField()
-    votes=models.IntegerField()
-    text=models.CharField(max_length=300)
-    image=models.CharField(max_length=300, null=True)
-    comments = models.ManyToManyField(Comment)
-
-
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, default=0)
