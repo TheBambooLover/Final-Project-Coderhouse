fd4d1f0b0abf47139462c112c4d4a4ffebad47c5
Tomas Oldenburg <oldenburgtomas@hotmail.com>
2022-11-02 22:28:08 -0300

    writters menu button

--- project/FinalProject/AppFinalProject/urls.py
+++ project/FinalProject/AppFinalProject/urls.py
@@ -6,12 +6,13 @@ urlpatterns = [
     path('home/',views.home,name='home'),
     path('posts/',views.posts,name='posts'), #all posts
     path('about/',views.about,name='about'),
+    path('writters',views.show_writters,name='writters'),
     path('users/',views.show_users),
     path('search-users',views.BuscarUser.as_view()),
     path('user-create',views.CreateUser.as_view()),
     path('post-create',views.CreatePost.as_view()),
     path('comment-create',views.CreateComment.as_view()),
     path('writter-create',views.CreateWritter.as_view()),
-    path('writters',views.show_writters),
+    path('writters',views.show_writters,name='writters'),
 
 ]
