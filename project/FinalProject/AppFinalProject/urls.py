f43ba8ba38cb31d6e2b55acd071e20f99512c72c
Tomas Oldenburg <oldenburgtomas@hotmail.com>
2022-11-07 22:05:59 -0300

    from function to class

--- project/FinalProject/AppFinalProject/urls.py
+++ project/FinalProject/AppFinalProject/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('posts/',views.posts,name='posts'), #all posts
     path('about/',views.about,name='about'),
     path('writters',views.show_writters,name='writters'),
-    path('users/',views.show_users),
+    path('user_list/',views.UsersList.as_view()),
     path('search-users',views.BuscarUser.as_view()),
     path('create-user',views.CreateUser.as_view()),
     path('create-post',views.CreatePost.as_view()),
--- project/FinalProject/AppFinalProject/views.py
+++ project/FinalProject/AppFinalProject/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from AppFinalProject.models import User
 from AppFinalProject.forms import Buscar, CommentForm, PostForm, UserForm,Post, WritterForm
 from django.views import View
+from django.views.generic import ListView
 
 def home(request):
     return render(request, "AppFinalProject/home.html")
@@ -13,9 +14,8 @@ def posts(request):
 def about(request):
         return render(request, "AppFinalProject/about.html")
 
-def show_users(request):
-  users = User.objects.all()
-  return render(request, "AppFinalProject/users.html", {"users": users})
+class UsersList(ListView):
+    model = User
 
 def show_writters(request):
     writters = User.objects.filter(group_id=3).all()
